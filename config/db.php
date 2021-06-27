1fa4557ab5c46375bb2b179364106a1c78811c48
Dranzd Viper <dranzd@gmail.com>
2021-06-27 16:05:38 +0800

    Use defined method to check if constants are defined

--- config/db.php
+++ config/db.php
@@ -3,9 +3,9 @@
 require_once 'db.local.php';
 
 return [
-    'host'     => DB_HOST ?? 'localhost',
-    'username' => DB_USERNAME ?? 'username',
-    'password' => DB_PASSWORD ?? 'password',
-    'database' => DB_NAME ?? 'database',
-    'port'     => DB_PORT ?? 'port',
+    'host'     => defined('DB_HOST') ? DB_HOST : 'localhost',
+    'username' => defined('DB_USERNAME') ? DB_USERNAME : 'username',
+    'password' => defined('DB_PASSWORD') ? DB_PASSWORD : 'password',
+    'database' => defined('DB_NAME') ? DB_NAME : 'database',
+    'port'     => defined('DB_PORT') ? DB_PORT : 'port',
 ];
